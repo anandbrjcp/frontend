0a5afb752ac867d160666fa3c567080c3f29544b
i349061 <a.ramu@sap.com>
2022-12-26 07:49:25 +0100

    production server Dockerfile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bye there!
+          created with docker compose
         </p>
         <a
           className="App-link"
